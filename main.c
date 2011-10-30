c580f26e37ef4ea3b3eb5a1e04675e88b76bef65
Michaël Mouchous <mick111@greed.home>
2011-10-31 00:28:42 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ThanksCathCass
+//
+//  Created by Michaël Mouchous on 31/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+        // insert code here...
+        printf("Hello, World!\n");
+    return 0;
+}
+
